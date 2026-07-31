ed2f7e8c26006ff9284279702708797b46d590d6
Miriam Polzer <mpolzer@google.com>
2026-07-31 14:12:18 +0200

    Improve ECC benchmarks
    ECDSAVerify is non-constant time, benchmark against a new sample on each
    run to get more accurate results.
    
    ECDH was derving a key, and manually calling multiplication in its
    benchmarks. It should be closer to reality to take key generation out of
    the benchmarking and run ECDH_compute_key.
    
    Add DoNotOptimize annd ClobberMemory to avoid optimizations.
    
    Increase digest length to avoid zero-padding for sign and verify.
    
    Change-Id: I3d27be2fdcb53e9e80e54f924a09f857d65db1cf
    Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/99847
    Reviewed-by: Rudolf Polzer <rpolzer@google.com>
    Commit-Queue: Miriam Polzer <mpolzer@google.com>

--- bench/ecdh.cc
+++ bench/ecdh.cc
@@ -12,78 +12,70 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <vector>
-
 #include <benchmark/benchmark.h>
 
 #include <openssl/base.h>
-#include <openssl/bn.h>
 #include <openssl/ec.h>
+#include <openssl/ecdh.h>
 #include <openssl/err.h>
 #include <openssl/evp.h>
 #include <openssl/mem.h>
 
 #include "./internal.h"
 
+BSSL_NAMESPACE_BEGIN
 namespace {
+
 void BM_SpeedECDH(benchmark::State &state, const EC_GROUP *group) {
-  bssl::UniquePtr<EC_KEY> peer_key(EC_KEY_new());
+  UniquePtr<EC_KEY> peer_key(EC_KEY_new());
   if (!peer_key || !EC_KEY_set_group(peer_key.get(), group) ||
       !EC_KEY_generate_key(peer_key.get())) {
     state.SkipWithError("peer keygen failed.");
     return;
   }
 
-  size_t peer_value_len =
-      EC_POINT_point2oct(group, EC_KEY_get0_public_key(peer_key.get()),
-                         POINT_CONVERSION_UNCOMPRESSED, nullptr, 0, nullptr);
-  if (peer_value_len == 0) {
-    state.SkipWithError("EC_POINT_point2oct failed.");
+  const EC_POINT *peer_pub = EC_KEY_get0_public_key(peer_key.get());
+
+  UniquePtr<EC_KEY> key(EC_KEY_new());
+  if (!key || !EC_KEY_set_group(key.get(), group) ||
+      !EC_KEY_generate_key(key.get())) {
+    state.SkipWithError("self keygen failed.");
     return;
   }
-  std::vector<uint8_t> peer_value(peer_value_len);
-  peer_value_len =
-      EC_POINT_point2oct(group, EC_KEY_get0_public_key(peer_key.get()),
-                         POINT_CONVERSION_UNCOMPRESSED, peer_value.data(),
-                         peer_value_len, nullptr);
-  if (peer_value_len == 0) {
-    state.SkipWithError("peer pubkey serialisation failed.");
+
+  uint8_t secret[66];
+  size_t secret_len = (EC_GROUP_get_degree(group) + 7) / 8;
+  if (secret_len > sizeof(secret)) {
+    state.SkipWithError("secret length exceeds buffer size.");
     return;
   }
 
   for (auto _ : state) {
-    bssl::UniquePtr<EC_KEY> key(EC_KEY_new());
-    if (!key || !EC_KEY_set_group(key.get(), group) ||
-        !EC_KEY_generate_key(key.get())) {
-      state.SkipWithError("self keygen failed.");
+    benchmark::ClobberMemory();
+    benchmark::DoNotOptimize(peer_pub);
+
+    int res =
+        ECDH_compute_key(secret, secret_len, peer_pub, key.get(), nullptr);
+    if (res < 0) {
+      state.SkipWithError("ECDH_compute_key failed.");
       return;
     }
-    bssl::UniquePtr<EC_POINT> point(EC_POINT_new(group));
-    bssl::UniquePtr<EC_POINT> peer_point(EC_POINT_new(group));
-    bssl::UniquePtr<BN_CTX> ctx(BN_CTX_new());
-    bssl::UniquePtr<BIGNUM> x(BN_new());
-    if (!point || !peer_point || !ctx || !x ||
-        !EC_POINT_oct2point(group, peer_point.get(), peer_value.data(),
-                            peer_value_len, ctx.get()) ||
-        !EC_POINT_mul(group, point.get(), nullptr, peer_point.get(),
-                      EC_KEY_get0_private_key(key.get()), ctx.get()) ||
-        !EC_POINT_get_affine_coordinates_GFp(group, point.get(), x.get(),
-                                             nullptr, ctx.get())) {
-      state.SkipWithError("final key agreement failed.");
-      return;
-    }
+
+    benchmark::DoNotOptimize(secret);
+    benchmark::DoNotOptimize(res);
   }
 }
 
 BSSL_BENCH_LAZY_REGISTER() {
   BENCHMARK_CAPTURE(BM_SpeedECDH, p224, EC_group_p224())
-      ->Apply(bssl::bench::SetThreads);
+      ->Apply(bench::SetThreads);
   BENCHMARK_CAPTURE(BM_SpeedECDH, p256, EC_group_p256())
-      ->Apply(bssl::bench::SetThreads);
+      ->Apply(bench::SetThreads);
   BENCHMARK_CAPTURE(BM_SpeedECDH, p384, EC_group_p384())
-      ->Apply(bssl::bench::SetThreads);
+      ->Apply(bench::SetThreads);
   BENCHMARK_CAPTURE(BM_SpeedECDH, p521, EC_group_p521())
-      ->Apply(bssl::bench::SetThreads);
+      ->Apply(bench::SetThreads);
 }
 
 }  // namespace
+BSSL_NAMESPACE_END
